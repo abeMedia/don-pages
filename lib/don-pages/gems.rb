93b7c2dca2176c43f0a859bb64e4875a92e64a85
Adam Bouqdib <adam@abemedia.co.uk>
2016-12-22 05:54:18 +0100

    Switch to less-execjs & drop therubyracer dependency, remove image_optim_pack & update html-proofer

--- lib/don-pages/gems.rb
+++ lib/don-pages/gems.rb
@@ -3,22 +3,21 @@ module DonPages
   GEMS = {
     # Jekyll
     "jekyll" => "3.3.1",
+    "html-proofer" => "3.4.0",
 
     # Misc
     "kramdown" => "1.13.1",
     "liquid" => "3.0.6",
     "rouge" => "1.11.1",
-    "html-proofer" => "3.3.1",
 
     # Assets
     "jekyll-assets" => "2.2.8",
     "autoprefixer-rails" => "6.5.4",
-    "less" => "2.6.0",
-    "therubyracer" => "0.12.2",
+    "less-execjs" => "2.6.0.3",
     "uglifier" => "3.0.4",
     "mini_magick" => "4.6.0",
     "image_optim" => "0.24.1",
-    "image_optim_pack" => "0.3.0.20161206",
+    # "image_optim_pack" => "0.3.0.20161206",
 
     # Plugins
     "jekyll-redirect-from" => "0.11.0",
